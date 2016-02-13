8403c85c30b3d2ff123f6bdc1ed5100cf935bcc4
Vincent <vincent@vincentdavis.net>
2016-02-13 07:52:10 -0700

    Update settings_secret_dev.py

--- classicbritish/settings_secret_dev.py
+++ classicbritish/settings_secret_dev.py
@@ -1,6 +1,11 @@
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
