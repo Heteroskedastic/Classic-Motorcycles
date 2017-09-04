82754f82373cf4acbcc8f83d8ff1e00f5fa0fe88
pprolancer <pprolancer@gmail.com>
2017-09-04 12:24:07 -0400

    - fixed the settings

--- classicmotor/manage.py
+++ classicmotor/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "classicmotor.settings_prod")
+                          "classicmotor.settings.main")
 
     from django.core.management import execute_from_command_line
 
